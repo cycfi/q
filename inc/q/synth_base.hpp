623e8c5d2bed04498686f81de39355875c058038
djowel <djowel@gmail.com>
2017-11-16 12:07:58 +0800

    allow phase to be negative; fix sin-table to mask the highest (sign) bit

--- inc/q/synth_base.hpp
+++ inc/q/synth_base.hpp
@@ -130,7 +130,7 @@ namespace cycfi { namespace q
    template <typename Freq, typename Shift>
    inline phase_t synth_base<Freq, Shift>::get() const
    {
-      return (_phase + shift()) & 0x7FFFFFFF;
+      return _phase + shift();
    }
 
    template <typename Freq, typename Shift>
