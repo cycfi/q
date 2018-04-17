d5c7e445016a49eef529974ed35647df81b55a81
djowel <djowel@gmail.com>
2018-04-18 07:07:40 +0800

    - cleanup and refactoring - generate non-integer harmonics

--- test/gen_harmonics.cpp
+++ test/gen_harmonics.cpp
@@ -16,20 +16,28 @@ using namespace q::literals;
 auto constexpr sps = 44100;
 auto constexpr buffer_size = sps;
 
-int main()
+constexpr q::frequency freq = 261.626; // 261.626;
+constexpr float _1st_level = 0.3;      // Fundamental level
+constexpr float _2nd_level = 0.4;      // Second harmonic level
+constexpr float _3rd_level = 0.3;      // Third harmonic level
+
+void write(std::string name, std::array<float, buffer_size>& buff)
+{
+   auto wav = audio_file::writer{
+      "results/" + name + ".wav", audio_file::wav, audio_file::_16_bits
+    , 1, sps // mono, 44100 sps
+   };
+   wav.write(buff);
+}
+
+void gen_harmonics_1(std::array<float, buffer_size>& buff)
 {
    ////////////////////////////////////////////////////////////////////////////
    // Synthesize a 1-second 261.626 Hz sine wave with harmonics
 
-   constexpr q::frequency freq = 261.626; // 261.626;
-   constexpr float _1st_level = 0.3;      // Fundamental level
-   constexpr float _2nd_level = 0.4;      // Second harmonic level
-   constexpr float _3rd_level = 0.3;      // Third harmonic level
-
    auto sin1 = q::sin(freq, sps);
    auto sin2 = q::sin(freq * 2, sps);
    auto sin3 = q::sin(freq * 3, sps);
-   auto buff = std::array<float, buffer_size>{};
    for (auto& val : buff)
       val =
          _1st_level * sin1() +
@@ -37,14 +45,33 @@ int main()
          _3rd_level * sin3()
       ;
 
-   ////////////////////////////////////////////////////////////////////////////
-   // Write to a wav file
+   write("harmonics_261", buff);
+}
 
-   auto wav = audio_file::writer{
-      "results/harmonics_261.wav", audio_file::wav, audio_file::_16_bits
-    , 1, sps // mono, 44100 sps
-   };
-   wav.write(buff);
+void gen_harmonics_2(std::array<float, buffer_size>& buff)
+{
+   ////////////////////////////////////////////////////////////////////////////
+   // Synthesize a 1-second 261.626 Hz sine wave with non-integer harmonics
+
+   auto sin1 = q::sin(freq, sps);
+   auto sin2 = q::sin(freq * 2.01, sps);
+   auto sin3 = q::sin(freq * 3.02, sps);
+   for (auto& val : buff)
+      val =
+         _1st_level * sin1() +
+         _2nd_level * sin2() +
+         _3rd_level * sin3()
+      ;
+
+   write("non_int_harmonics_261", buff);
+}
+
+int main()
+{
+   auto buff = std::array<float, buffer_size>{};
+
+   gen_harmonics_1(buff);
+   gen_harmonics_2(buff);
 
    return 0;
 }
