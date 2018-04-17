77934720e5e79d78f4b797ca75f9b104a2a6168c
djowel <djowel@gmail.com>
2018-04-18 07:06:55 +0800

    cleanup and refactoring

--- test/pitch_detection.cpp
+++ test/pitch_detection.cpp
@@ -6,31 +6,25 @@
 =============================================================================*/
 #include <q/literals.hpp>
 #include <q/sfx.hpp>
-#include <q_io/audio_file.hpp>
 #include <q/pitch_detector.hpp>
 #include <q/pitch_detector.hpp>
 #include <vector>
 #include <iostream>
 
 namespace q = cycfi::q;
-namespace audio_file = q::audio_file;
 using namespace q::literals;
 
-constexpr auto pi = M_PI;
-constexpr auto sps = 44100;               // 20000;
+constexpr auto pi = q::pi;
+constexpr auto sps = 44100;
 
 void process(
-   std::vector<float> const& in
- , std::string name
+   std::vector<float>&& in
+ , q::frequency actual_frequency
  , q::frequency lowest_freq
  , q::frequency highest_freq)
 {
    ////////////////////////////////////////////////////////////////////////////
    // Process
-   constexpr auto n_channels = 2;
-   std::vector<float> out(in.size() * n_channels);
-   std::fill(out.begin(), out.end(), 0);
-
    q::pitch_detector<> pd{ lowest_freq, highest_freq, sps };
    auto const& bacf = pd.bacf();
    auto size = bacf.size();
@@ -43,26 +37,14 @@ void process(
    for (auto i = 0; i != in.size(); ++i)
    {
       auto s = in[i];
-      out[i * n_channels] = s;
-      out[(i * n_channels) + 1] = -1; // placeholder
 
       // Pitch Detection
       bool proc = pd(s);
 
       if (proc)
       {
-         auto pos = i - size;
-         auto oi = (out.begin() + pos * n_channels) + 1;
-
-         auto const& info = bacf.result();
-         for (auto n : info.correlation)
-         {
-            *oi = n / float(info.max_count);
-            oi += n_channels;
-         }
-
          auto frequency = pd.frequency();
-         auto error = 1200.0 * std::log2(frequency / 261.626);
+         auto error = 1200.0 * std::log2(frequency / double(actual_frequency));
          std::cout
             << fixed
             << frequency
@@ -83,45 +65,32 @@ void process(
    std::cout << fixed << "Average Error: " << ave_error << " cents." << std::endl;
    std::cout << fixed << "Min Error: " << min_error << " cents." << std::endl;
    std::cout << fixed << "Max Error: " << max_error << " cents." << std::endl;
-
-   ////////////////////////////////////////////////////////////////////////////
-   // Write to a wav file
-
-   auto wav = audio_file::writer{
-      "results/pitch_detect_" + name + ".wav", audio_file::wav, audio_file::_16_bits
-    , n_channels, sps
-   };
-   wav.write(out);
 }
 
-int main()
+std::vector<float> harmonics(q::frequency freq)
 {
-   constexpr float freq = 261.626;           // 82.41;
+   constexpr float _1st_level = 0.3;   // Fundamental level
+   constexpr float _2nd_level = 0.4;   // Second harmonic level
+   constexpr float _3rd_level = 0.3;   // Third harmonic level
 
-   // These are in samples
-   constexpr float period = float(sps) / freq;
-
-   ////////////////////////////////////////////////////////////////////////////
-   // Generate a test signal
-
-   constexpr float _1st_level = 0.3;      // Fundamental level
-   constexpr float _2nd_level = 0.4;      // Second harmonic level
-   constexpr float _3rd_level = 0.3;      // Third harmonic level
-
+   auto period = double(sps / freq);
    constexpr float offset = 100;
    std::size_t buff_size = 10000;
 
    std::vector<float> signal(buff_size);
-
    for (int i = 0; i < buff_size; i++)
    {
       auto angle = (i + offset) / period;
-      signal[i] += _1st_level *  std::sin(2 * pi * angle);  // First harmonic
-      signal[i] += _2nd_level *  std::sin(4 * pi * angle);  // Second harmonic
-      signal[i] += _3rd_level *  std::sin(6 * pi * angle);  // Third harmonic
+      signal[i] += _1st_level * std::sin(2 * pi * angle);   // First harmonic
+      signal[i] += _2nd_level * std::sin(4 * pi * angle);   // Second harmonic
+      signal[i] += _3rd_level * std::sin(6 * pi * angle);   // Third harmonic
    }
+   return signal;
+}
 
-   process(signal, "harmonics_261", 200_Hz, 1000_Hz);
+int main()
+{
+   process(harmonics(261.626_Hz), 261.626_Hz, 200_Hz, 1000_Hz);
    return 0;
 }
 
