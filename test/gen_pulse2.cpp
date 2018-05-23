a8cf09f7772f40ddda15972b79c045a3be38ef87
djowel <djowel@gmail.com>
2018-05-23 15:53:30 +0800

    Added envelope-modulated pulse synth test

--- test/gen_pulse2.cpp
+++ test/gen_pulse2.cpp
@@ -20,13 +20,13 @@ constexpr auto buffer_size = sps * 10;
 int main()
 {
    ////////////////////////////////////////////////////////////////////////////
-   // Synthesize a 10-second sine wave with ADSR envelope
+   // Synthesize a 10-second pulse wave with ADSR envelope
 
    // Our envelope
    auto env =
       q::envelope(
         10_ms     // attack rate
-      , 80_ms     // decay rate
+      , 200_ms    // decay rate
       , -6_dB     // sustain level
       , 10_s      // sustain rate
       , 0.5_s     // release rate
@@ -37,13 +37,16 @@ int main()
    constexpr auto f = q::phase(440_Hz, sps);       // The synth frequency
    auto ph = q::phase();                           // Our phase accumulator
 
+   auto pulse = q::pulse;                          // Our pulse synth
+
    env.trigger();                                  // Trigger note
    for (auto i = 0; i != buffer_size; ++i)
    {
       auto& val = buff[i];
       if (i == buffer_size/2)                      // Release note
          env.release();
-      val = q::sin(ph) * env();
+      pulse.width((env() * 0.6) + 0.3);            // Set pulse width
+      val = pulse(ph, f) * env();
       ph += f;
    }
 
@@ -51,7 +54,7 @@ int main()
    // Write to a wav file
 
    auto wav = audio_file::writer{
-      "results/gen_sin2.wav", audio_file::wav, audio_file::_16_bits
+      "results/gen_pulse2.wav", audio_file::wav, audio_file::_16_bits
     , 1, sps // mono, 48000 sps
    };
    wav.write(buff);
