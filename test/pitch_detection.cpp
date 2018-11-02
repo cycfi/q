5b1b9dc309cbf64b1dffb6bb8c8d60734d7e134e
djowel <djowel@gmail.com>
2018-11-03 00:19:38 +0800

    some cleanup

--- test/pitch_detection.cpp
+++ test/pitch_detection.cpp
@@ -188,11 +188,16 @@ void process(
    }
 
    CHECK_MESSAGE(result.ave_error < ave_error_expected,
-      "Average error exceeded: " << result.ave_error << " expecting: " << ave_error_expected);
+      "Average error exceeded: " << result.ave_error <<
+      " expecting: " << ave_error_expected);
+
    CHECK_MESSAGE(result.min_error < min_error_expected,
-      "Minimum error exceeded: " << result.min_error << " expecting: " << min_error_expected);
+      "Minimum error exceeded: " << result.min_error <<
+      " expecting: " << min_error_expected);
+
    CHECK_MESSAGE(result.max_error < max_error_expected,
-      "Maximum error exceeded: " << result.max_error << " expecting: " << max_error_expected);
+      "Maximum error exceeded: " << result.max_error <<
+      " expecting: " << max_error_expected);
 }
 
 void process(
@@ -211,133 +216,151 @@ void process(
    );
 }
 
-
 using namespace notes;
-params params_;
 
 TEST_CASE("Test_middle_C")
 {
+   params params_;
    process(params_, middle_c, 200_Hz, 0.0011, 0.000097, 0.0024);
 }
 
 TEST_CASE("Test_middle_A")
 {
+   params params_;
    process(params_, 440_Hz, 200_Hz, 0.002, 0.0000001, 0.008);
 }
 
 TEST_CASE("Test_low_E")
 {
+   params params_;
    process(params_, low_e, low_e, 0.000035, 000035, 0.000035);
 }
 
 TEST_CASE("Test_E_12th")
 {
+   params params_;
    process(params_, low_e_12th, low_e, 0.00022, 0.000035, 0.00052);
 }
 
 TEST_CASE("Test_E_24th")
 {
+   params params_;
    process(params_, low_e_24th, low_e, 0.00023, 0.000035, 0.00052, "low_e_24th");
 }
 
 TEST_CASE("Test_A")
 {
+   params params_;
    process(params_, a, a, 0.000001, 0.000001, 0.000001);
 }
 
 TEST_CASE("Test_A_12th")
 {
+   params params_;
    process(params_, a_12th, a, 0.00019, 0.000001, 0.00073);
 }
 
 TEST_CASE("Test_A_24th")
 {
+   params params_;
    process(params_, a_24th, a, 0.00048, 0.000001, 0.0011);
 }
 
 TEST_CASE("Test_D")
 {
+   params params_;
    process(params_, d, d, 0.00018, 0.000022, 0.00034);
 }
 
 TEST_CASE("Test_D_12th")
 {
+   params params_;
    process(params_, d_12th, d, 0.00058, 0.000022, 0.0017);
 }
 
 TEST_CASE("Test_D_24th")
 {
+   params params_;
    process(params_, d_24th, d, 0.0060, 0.0015, 0.0094);
 }
 
 TEST_CASE("Test_G")
 {
+   params params_;
    process(params_, g, g, 0.000061, 0.000061, 0.000061);
 }
 
 TEST_CASE("Test_G_12th")
 {
+   params params_;
    process(params_, g_12th, g, 0.00018, 0.000061, 0.00021);
 }
 
 TEST_CASE("Test_G_24th")
 {
+   params params_;
    process(params_, g_24th, g, 0.00015, 0.000061, 0.0047);
 }
 
 TEST_CASE("Test_B")
 {
+   params params_;
    process(params_, b, b, 0.00053, 0.000003,  0.0015);
 }
 
 TEST_CASE("Test_B_12th")
 {
+   params params_;
    process(params_, b_12th, b, 0.0068, 0.00011, 0.012);
 }
 
 TEST_CASE("Test_B_24th")
 {
+   params params_;
    process(params_, b_24th, b, 0.0035, 0.00065, 0.0076);
 }
 
 TEST_CASE("Test_high_E")
 {
+   params params_;
    process(params_, high_e, high_e, 0.00068, 0.000035, 0.0021);
 }
 
 TEST_CASE("Test_high_E_12th")
 {
+   params params_;
    process(params_, high_e_12th, high_e, 0.0034, 0.000035, 0.0095);
 }
 
 TEST_CASE("Test_high_E_24th")
 {
+   params params_;
    process(params_, high_e_24th, high_e, 0.027, 0.000035, 0.044);
 }
 
 TEST_CASE("Test_non_integer_harmonics")
 {
+   params params_;
    params_._2nd_harmonic = 2.003;
    process(params_, low_e, low_e, 1.1, 0.95, 1.1, "non_integer");
-   params_ = params{};
 }
 
 TEST_CASE("Test_phase_offsets")
 {
+   params params_;
    params_._1st_offset = 0.1;
    params_._2nd_offset = 0.5;
    params_._3rd_offset = 0.4;
    process(params_, low_e, low_e, 0.00014, 0.000035, 0.00036, "phase_offset");
-   params_ = params{};
 }
 
 TEST_CASE("Test_missing_fundamental")
 {
+   params params_;
    params_._1st_level = 0.0;
    params_._2nd_level = 0.5;
    params_._3rd_level = 0.5;
    process(params_, low_e, low_e, 0.000035, 0.000035, 0.000035, "missing_fundamental");
-   params_ = params{};
 }
 
 
