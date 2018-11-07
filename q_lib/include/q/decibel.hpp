fd5f01990f3511219d993be7f87d0be6153c46d6
djowel <djowel@gmail.com>
2018-11-07 23:24:30 +0800

    improved decibel class/struct

--- q_lib/include/q/decibel.hpp
+++ q_lib/include/q/decibel.hpp
@@ -7,6 +7,7 @@
 #define CYCFI_Q_DECIBEL_HPP_FEBRUARY_21_2018
 
 #include <cmath>
+#include <q/support.hpp>
 
 namespace cycfi { namespace q
 {
@@ -15,15 +16,55 @@ namespace cycfi { namespace q
    {
       constexpr decibel(double val) : val(val) {}
 
-      operator double() const                { return std::pow(10.0, val/20.0); }
-      constexpr decibel operator-() const    { return {-val}; }
+      operator double() const                { return fast_pow10(val/20.0); }
+      constexpr decibel operator-() const    { return { -val }; }
 
       double val = 0.0f;
    };
 
    inline decibel to_db(double val)
    {
-      return 20 * std::log10(val);
+      return 20.0f * fast_log10(val);
+   }
+
+   constexpr decibel operator-(decibel a, decibel b)
+   {
+      return { a.val - b.val };
+   }
+
+   constexpr decibel operator+(decibel a, decibel b)
+   {
+      return { a.val + b.val };
+   }
+
+   constexpr bool operator==(decibel a, decibel b)
+   {
+      return a.val == b.val;
+   }
+
+   constexpr bool operator!=(decibel a, decibel b)
+   {
+      return a.val != b.val;
+   }
+
+   constexpr bool operator<(decibel a, decibel b)
+   {
+      return a.val < b.val;
+   }
+
+   constexpr bool operator<=(decibel a, decibel b)
+   {
+      return a.val <= b.val;
+   }
+
+   constexpr bool operator>(decibel a, decibel b)
+   {
+      return a.val > b.val;
+   }
+
+   constexpr bool operator>=(decibel a, decibel b)
+   {
+      return a.val >= b.val;
    }
 
    // $$$ TODO: Use fast approximate versions of std::log10 and std::pow $$$
