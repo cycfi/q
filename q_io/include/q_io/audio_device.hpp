9e0d81692a8a22b308069069bf326943aa610637
djowel <djowel@gmail.com>
2018-12-01 15:17:53 +0800

    added audio_device class

---
+++ q_io/include/q_io/audio_device.hpp
@@ -0,0 +1,40 @@
+/*=============================================================================
+   Copyright (c) 2014-2018 Joel de Guzman. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(CYCFI_Q_AUDIO_FILE_HPP_DECEMBER_1_2018)
+#define CYCFI_Q_AUDIO_FILE_HPP_DECEMBER_1_2018
+
+#include <set>
+#include <vector>
+
+namespace cycfi { namespace q
+{
+   ////////////////////////////////////////////////////////////////////////////
+   class audio_device
+   {
+   public:
+
+      enum io_dir { input, output };
+      using device_list = std::vector<audio_device>;
+
+      static device_list         list();
+      uint32_t                   id() const;
+      std::string                name() const;
+      std::size_t                num_channels() const;
+      io_dir                     direction() const;
+
+   private:
+
+      struct impl;
+      audio_device(impl const& impl)
+       : _impl(impl)
+      {}
+
+      impl const&                _impl;
+   };
+
+}}
+
+#endif
---
+++ q_io/src/audio_device.cpp
@@ -0,0 +1,80 @@
+/*=============================================================================
+   Copyright (c) 2014-2018 Joel de Guzman. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#include <q_io/audio_device.hpp>
+#include <portaudio.h>
+#include <string>
+
+namespace cycfi { namespace q
+{
+   struct audio_device::impl
+   {
+      using io_dir = audio_device::io_dir;
+
+      uint32_t             _id;
+      std::string          _name;
+      std::size_t          _num_channels;
+      io_dir               _direction;
+   };
+
+   uint32_t audio_device::id() const
+   {
+      return _impl._id;
+   }
+
+   std::string audio_device::name() const
+   {
+      return _impl._name;
+   }
+
+   std::size_t audio_device::num_channels() const
+   {
+      return _impl._num_channels;
+   }
+
+   audio_device::io_dir audio_device::direction() const
+   {
+      return _impl._direction;
+   }
+
+   std::vector<audio_device> audio_device::list()
+   {
+      int num_devices = Pa_GetDeviceCount();
+      if (num_devices < 0)
+         return {};
+
+      static std::vector<audio_device::impl> devices;
+      devices.clear();
+
+      PaDeviceInfo const* info;
+      for (auto i=0; i < num_devices; ++i)
+      {
+         info = Pa_GetDeviceInfo(i);
+         audio_device::impl impl;
+         impl._id = i;
+         impl._name = info->name;
+
+         if (info->maxInputChannels)
+         {
+            impl._num_channels = info->maxInputChannels;
+            impl._direction = input;
+            devices.push_back(impl);
+         }
+
+         if (info->maxOutputChannels)
+         {
+            impl._num_channels = info->maxOutputChannels;
+            impl._direction = output;
+            devices.push_back(impl);
+         }
+      }
+
+      std::vector<audio_device> result;
+      for (auto const& impl : devices)
+         result.push_back(impl);
+      return result;
+   }
+}}
+
