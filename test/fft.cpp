591775ca4abcbb60da4b5b6af31c2b27918c4a30
djowel <djowel@gmail.com>
2018-12-25 18:41:58 +0800

    bigger FFT size with result to wav file

--- test/fft.cpp
+++ test/fft.cpp
@@ -5,9 +5,6 @@
 =============================================================================*/
 #include <q/fft/fft.hpp>
 #include <q_io/audio_file.hpp>
-
-#include <iostream>
-#include <iomanip>
 #include <array>
 
 namespace q = cycfi::q;
@@ -17,27 +14,47 @@ constexpr auto sps = 48000;
 
 int main()
 {
-   constexpr std::size_t p = 4;
-
-   // sample data
+   constexpr std::size_t p = 10;
    constexpr std::size_t n = 1<<p;
+   constexpr auto n_channels = 3;
+   std::vector<float> out(n*2 * n_channels);
+
+   ////////////////////////////////////////////////////////////////////////////
+   // sample data
    std::array<double, n*2> data;
-   for (int i=0; i<n; ++i)
+   for (int i = 0; i < n*2; ++i)
    {
-      data[2*i] = 2*i;
-      data[2*i+1] = 2*i+1;
+      data[i] =
+         0.4 * std::sin(2_pi * i * 10 / (n * 2)) +
+         0.5 * std::sin(2_pi * i * 20 / (n * 2)) +
+         0.1 * std::sin(2_pi * i * 30 / (n * 2))
+      ;
+
+      auto pos = i * n_channels;
+      out[pos] = data[i];
    }
 
+   ////////////////////////////////////////////////////////////////////////////
    // compute FFT
    q::fft<n>(data.data());
 
-   // print the results
-   std::cout<<"--------------------------------" << std::endl;
-   for (int i = 0; i < n; ++i)
+   for (int i = 0; i < n*2; ++i)
    {
-      std::cout << std::setw(10) << std::setprecision(5) << data[2*i] << "\t"
-         << data[2*i+1] << "I" << std::endl;
+      auto pos = i * n_channels;
+      auto ch2 = pos+1;
+      auto ch3 = pos+2;
+
+      out[ch2] = data[i] / (n/2);
+      out[ch3] = data[i+1] / (n/2);
    }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Write to a wav file
+
+   q::wav_writer wav{
+      "results/fft.wav", n_channels, sps
+   };
+   wav.write(out);
 
    return 0;
 }
