e0bb7414f8619a8fa401d3d95f73a536b967159a
djowel <djowel@gmail.com>
2018-12-29 18:18:47 +0800

    Keep it as simple as possible!

--- q_lib/include/q/utility/zero_crossing.hpp
+++ q_lib/include/q/utility/zero_crossing.hpp
@@ -8,37 +8,42 @@
 
 #include <q/utility/bitstream.hpp>
 #include <q/utility/ring_buffer.hpp>
-#include <q/utility/bitstream.hpp>
-#include <q/detail/count_bits.hpp>
 #include <infra/assert.hpp>
 #include <cmath>
 
 namespace cycfi { namespace q
 {
    ////////////////////////////////////////////////////////////////////////////
-   // The zero_crossing class saves bit information for each sample in a
-   // bitstream (see bitstream.hpp) for performing analysis such as
-   // autocorrelation, as well as additional information about each zero
-   // crossing event necessary to extract accurate timing information such as
-   // periods between pulses.
+   // The zero_crossing class saves zero-crossing information necessary to
+   // extract accurate timing information such as periods between pulses for
+   // performing analysis such as autocorrelation.
+   //
+   // Each zero crossing pulse is saved in a buffer with info elements that
+   // can be accessed anytime. The data includes the maximum height of the
+   // waveform enclosed within the pulse as well as the x (frame index) and y
+   // positions (the sample values before and after each zero crossing) of
+   // the zero crossings.
+   //
+   // Only the latest few (finite amount) of zero crossing information is
+   // saved, given by the window constructor parameter.
+   //
+   // Each call to the function operator, given a sample s, returns the
+   // zero-crossing state (bool). is_ready() returns true when we have
+   // sufficient info to perform analysis. is_ready() returns true after
+   // every window/2 frames. Information about each zero crossing can be
+   // obtained using the index operator[].
    ////////////////////////////////////////////////////////////////////////////
    class zero_crossing
    {
    public:
-
-      // Each zero crossing pulse is saved in a buffer with info elements
-      // (below) which can be accessed anytime. The data includes the maximum
-      // height of the waveform enclosed within the pulse as well as the x
-      // (frame index) and y positions (the sample values before and after
-      // each zero crossing) of the zero crossings. Only the latest few
-      // (finite amount) of zero crossing information is saved, given by the
-      // window constructor parameter.
 
       struct info
       {
          using crossing_data = std::pair<float, float>;
 
          void              update_peak(float s);
+         std::size_t       period(info const& next) const;
+         float             fractional_period(info const& next) const;
 
          crossing_data     _crossing;
          float             _peak;
@@ -52,6 +57,7 @@ namespace cycfi { namespace q
       std::size_t          capacity() const;
       std::size_t          frame() const;
       std::size_t          window_size() const;
+      bool                 is_ready() const;
 
       bool                 operator()(float s);
       bool                 operator()() const;
@@ -60,7 +66,6 @@ namespace cycfi { namespace q
 
    private:
 
-      void                 estimate_period();
       void                 update_state(float s);
       void                 shift(std::size_t n);
       void                 reset();
@@ -74,6 +79,7 @@ namespace cycfi { namespace q
       std::size_t          _num_edges = 0;
       std::size_t const    _window_size;
       std::size_t          _frame = 0;
+      bool                 _ready = false;
    };
 
    ////////////////////////////////////////////////////////////////////////////
@@ -100,6 +106,33 @@ namespace cycfi { namespace q
       _peak = std::max(s, _peak);
    }
 
+   inline std::size_t zero_crossing::info::period(info const& next) const
+   {
+      CYCFI_ASSERT(_leading_edge <= next._leading_edge, "Invalid order.");
+      return next._leading_edge - _leading_edge;
+   }
+
+   inline float zero_crossing::info::fractional_period(info const& next) const
+   {
+      CYCFI_ASSERT(_leading_edge <= next._leading_edge, "Invalid order.");
+
+      // Get the start edge
+      auto prev1 = _crossing.first;
+      auto curr1 = _crossing.second;
+      auto dy1 = curr1 - prev1;
+      auto dx1 = -prev1 / dy1;
+
+      // Get the next edge
+      auto prev2 = next._crossing.first;
+      auto curr2 = next._crossing.second;
+      auto dy2 = curr2 - prev2;
+      auto dx2 = -prev2 / dy2;
+
+      // Calculate the fractional period
+      auto result = next._leading_edge - _leading_edge;
+      return result + (dx2 - dx1);
+   }
+
    inline std::size_t zero_crossing::num_edges() const
    {
       return _num_edges;
@@ -120,15 +153,26 @@ namespace cycfi { namespace q
       return _window_size;
    }
 
-   void zero_crossing::reset()
+   inline void zero_crossing::reset()
    {
       _num_edges = 0;
       _state = false;
       _frame = 0;
    }
 
+   inline bool zero_crossing::is_ready() const
+   {
+      return _ready;
+   }
+
    inline void zero_crossing::update_state(float s)
    {
+      if (_ready)
+      {
+         shift(_window_size / 2);
+         _ready = false;
+      }
+
       if (num_edges() >= capacity())
          reset();
 
@@ -154,44 +198,24 @@ namespace cycfi { namespace q
       _prev = s;
    }
 
-   inline void zero_crossing::estimate_period()
-   {
-      if (++_frame >= _window_size && !_state)
-      {
-         auto half = _window_size / 2;
-
-         // Remove half the size from _frame, so we can continue seamlessly
-         _frame -= half;
-
-         // We need at least two rising edges.
-         if (num_edges() > 1)
-         {
-            bitstream<> _bits{ _window_size };
-            for (auto i = 0; i != num_edges(); ++i)
-            {
-               auto& info = (*this)[i];
-               auto pos = std::max<int>(info._leading_edge, 0);
-               auto n = info._trailing_edge - pos;
-               _bits.set(pos, n, 1);
-            }
-
-            // Shift the edges by half the number of samples
-            shift(half);
-         }
-         else
-         {
-            reset();
-         }
-      }
-   }
-
    inline bool zero_crossing::operator()(float s)
    {
       if (num_edges() >= capacity())
          reset();
 
       update_state(s);
-      estimate_period();
+
+      if (++_frame >= _window_size && !_state)
+      {
+         // Remove half the size from _frame, so we can continue seamlessly
+         _frame -= _window_size / 2;
+
+         // We need at least two rising edges.
+         if (num_edges() > 1)
+            _ready = true;
+         else
+            reset();
+      }
       return _state;
    };
 
@@ -200,12 +224,14 @@ namespace cycfi { namespace q
       return _state;
    }
 
-   inline zero_crossing::info const& zero_crossing::operator[](std::size_t index) const
+   inline zero_crossing::info const&
+   zero_crossing::operator[](std::size_t index) const
    {
       return _info[(_num_edges-1)-index];
    }
 
-   inline zero_crossing::info& zero_crossing::operator[](std::size_t index)
+   inline zero_crossing::info&
+   zero_crossing::operator[](std::size_t index)
    {
       return _info[(_num_edges-1)-index];
    }
@@ -225,50 +251,6 @@ namespace cycfi { namespace q
       }
       _num_edges = i;
    }
-
-   struct auto_correlator
-   {
-      static constexpr auto value_size = bitstream<>::value_size;
-
-      auto_correlator(bitstream<> const& bits)
-       : _bits(bits)
-       , _size(bits.size())
-       , _mid_array(((_size / value_size) / 2) - 1)
-      {}
-
-      std::size_t mid_point() const { return _size / 2; }
-
-      inline std::size_t operator()(std::size_t pos)
-      {
-         auto const index = pos / value_size;
-         auto const shift = pos % value_size;
-
-         auto const* p1 = _bits.data();
-         auto const* p2 = _bits.data() + index;
-         auto count = 0;
-
-         if (shift == 0)
-         {
-            for (auto i = 0; i != _mid_array; ++i)
-               count += detail::count_bits(*p1++ ^ *p2++);
-         }
-         else
-         {
-            auto shift2 = value_size - shift;
-            for (auto i = 0; i != _mid_array; ++i)
-            {
-               auto v = *p2++ >> shift;
-               v |= *p2 << shift2;
-               count += detail::count_bits(*p1++ ^ v);
-            }
-         }
-         return count;
-      };
-
-      bitstream<> const&   _bits;
-      std::size_t const    _size;
-      std::size_t const    _mid_array;
-   };
 }}
 
 #endif
