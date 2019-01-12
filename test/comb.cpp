69a57ace712ed572d133015713ac483b5c79ad3f
djowel <djowel@gmail.com>
2019-01-12 17:37:58 +0800

    Added comb filter test

---
+++ test/comb.cpp
@@ -0,0 +1,75 @@
+/*=============================================================================
+   Copyright (c) 2014-2018 Joel de Guzman. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#include <q/support/literals.hpp>
+#include <q_io/audio_file.hpp>
+#include <q/fx/delay.hpp>
+#include <vector>
+#include <string>
+#include "notes.hpp"
+
+namespace q = cycfi::q;
+using namespace q::literals;
+using namespace notes;
+
+void process(
+   std::string name, std::vector<float> const& in
+ , std::uint32_t sps, std::size_t samples)
+{
+   constexpr auto n_channels = 2;
+   std::vector<float> out(in.size() * n_channels);
+
+   auto dly = q::delay{ samples, sps };
+
+   for (auto i = 0; i != in.size(); ++i)
+   {
+      auto pos = i * n_channels;
+      auto ch1 = pos;
+      auto ch2 = pos+1;
+
+      auto s = in[i];
+
+      // Original signal
+      out[ch1] = s;
+
+      // Comb
+      out[ch2] = s + -dly(s, samples / 4);
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Write to a wav file
+
+   q::wav_writer wav{
+      "results/comb_" + name + ".wav", n_channels, sps
+   };
+   wav.write(out);
+}
+
+void process(std::string name, q::frequency f)
+{
+   ////////////////////////////////////////////////////////////////////////////
+   // Read audio file
+
+   q::wav_reader src{"audio_files/" + name + ".wav"};
+   std::uint32_t const sps = src.sps();
+
+   std::vector<float> in(src.length());
+   src.read(in);
+
+   ////////////////////////////////////////////////////////////////////////////
+   process(name, in, sps, float(f.period()) * sps);
+}
+
+int main()
+{
+   process("1a-Low-E", low_e);
+   process("1b-Low-E-12th", low_e);
+   process("Tapping D", d);
+   process("Hammer-Pull High E", high_e);
+   process("Bend-Slide G", g);
+   process("GStaccato", g);
+
+   return 0;
+}
