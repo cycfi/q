ea2cffd4b540f6ce1cbb71f2b6ef418698d7eda4
djowel <djowel@gmail.com>
2019-11-01 16:08:04 +0800

    Added conversion from string to MIDI note-number

--- q_lib/include/q/support/midi.hpp
+++ q_lib/include/q/support/midi.hpp
@@ -458,16 +458,16 @@ namespace cycfi { namespace q { namespace midi
    {
       constexpr char const* name[] =
       {
-         "C-1", "C#-1", "D-1", "D#-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "A#-1", "B-1", 
-         "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0", 
-         "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1", 
-         "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", 
-         "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", 
-         "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", 
-         "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", 
-         "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6", 
-         "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7", 
-         "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8", 
+         "C-1", "C#-1", "D-1", "D#-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "A#-1", "B-1",
+         "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
+         "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+         "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+         "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+         "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+         "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+         "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+         "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
+         "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8",
          "C9", "C#9", "D9", "D#9", "E9", "F9", "F#9", "G9"
       };
 
@@ -605,6 +605,60 @@ namespace cycfi { namespace q { namespace midi
     , Fs9, Gb9 = Fs9
     , G9
    };
+
+   ////////////////////////////////////////////////////////////////////////////
+   // Key (e.g. "C5") to MIDI note number
+   //
+   // Returns the MIDI note number given the key (string).
+   // Returns -1 if parsing failed.
+   ////////////////////////////////////////////////////////////////////////////
+   inline int note_number(std::string_view note)
+   {
+      std::uint8_t n;
+      auto iter = note.begin();
+      if (iter != note.end())
+      {
+         // Get the base frequency
+         switch (std::toupper(*iter++))
+         {
+            case 'A':   n = int(note::A0); break;
+            case 'B':   n = int(note::B0); break;
+            case 'C':   n = int(note::C0); break;
+            case 'D':   n = int(note::D0); break;
+            case 'E':   n = int(note::E0); break;
+            case 'F':   n = int(note::F0); break;
+            case 'G':   n = int(note::G0); break;
+         }
+
+         if (iter != note.end())
+         {
+            // See if we want to shift up or down by a
+            // semitone.
+            if (*iter == '#')
+            {
+               ++n;
+               ++iter;
+            }
+            else if (*iter == 'b')
+            {
+               --n;
+               ++iter;
+            }
+
+            if (iter != note.end())
+            {
+               if (std::isdigit(*iter))
+               {
+                  // Set the octave
+                  int oct = *iter++ - '0';
+                  if (iter == note.end())
+                     return n + oct * 12;
+               }
+            }
+         }
+      }
+      return -1;
+   }
 
    ////////////////////////////////////////////////////////////////////////////
    // processor
