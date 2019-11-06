cbb0c9ca936983ccf89d31fe0dd6a33277f0a393
djowel <djowel@gmail.com>
2019-11-06 20:59:24 +0800

    more improvements

--- test/onset_detector.cpp
+++ test/onset_detector.cpp
@@ -10,20 +10,18 @@
 #include <q/fx/feature_detection.hpp>
 #include <q/fx/special.hpp>
 #include <vector>
+#include "notes.hpp"
 
 namespace q = cycfi::q;
 using namespace q::literals;
+using namespace notes;
 
-void process(std::string name)
+void process(
+   std::string name, std::vector<float> const& in
+ , std::uint32_t sps, std::size_t n)
 {
-   ////////////////////////////////////////////////////////////////////////////
-   // Read audio file
-
-   q::wav_reader src{"audio_files/" + name + ".wav"};
-   std::uint32_t const sps = src.sps();
-
-   std::vector<float> in(src.length());
-   src.read(in);
+   constexpr auto n_channels = 4;
+   std::vector<float> out(in.size() * n_channels);
 
    auto max_val = *std::max_element(in.begin(), in.end(),
       [](auto a, auto b) { return std::abs(a) < std::abs(b); }
@@ -32,17 +30,15 @@ void process(std::string name)
    ////////////////////////////////////////////////////////////////////////////
    // Detect waveform peaks
 
-   constexpr auto n_channels = 4;
-   std::vector<float> out(src.length() * n_channels);
    auto i = out.begin();
 
    auto _diff1 = q::central_difference{};
    auto _diff2 = q::differentiator{};
-   auto _fast_env1 = q::fast_envelope_follower{ 4_ms, sps };
-   auto _fast_env2 = q::fast_envelope_follower{ 4_ms, sps };
+   auto _fast_env1 = q::fast_envelope_follower{ n };
+   auto _fast_env2 = q::fast_envelope_follower{ n };
    auto _env = q::envelope_follower{ 10_ms, 50_ms, sps };
    auto _cmp = q::schmitt_trigger{ -36_dB };
-   auto _pulse = q::pulse{ 15_ms, sps };
+   auto _pulse = q::pulse{ 25_ms, sps };
 
    for (auto s : in)
    {
@@ -78,13 +74,33 @@ void process(std::string name)
    wav.write(out);
 }
 
+void process(std::string name, q::frequency f)
+{
+   ////////////////////////////////////////////////////////////////////////////
+   // Read audio file
+
+   q::wav_reader src{"audio_files/" + name + ".wav"};
+   std::uint32_t const sps = src.sps();
+
+   std::vector<float> in(src.length());
+   src.read(in);
+
+   ////////////////////////////////////////////////////////////////////////////
+   auto period = f.period();
+   std::size_t n = float(period) * sps;
+   process(name, in, sps, n * 1.1);
+}
+
 int main()
 {
-   process("1a-Low-E");
-   process("Tapping D");
-   process("Hammer-Pull High E");
-   process("Bend-Slide G");
-   process("GStaccato");
+   process("1a-Low-E", low_e);
+   process("1b-Low-E-12th", low_e);
+   process("Tapping D", d);
+   process("Hammer-Pull High E", high_e);
+   process("Bend-Slide G", g);
+   process("GStaccato", g);
+
    return 0;
 }
+
 
