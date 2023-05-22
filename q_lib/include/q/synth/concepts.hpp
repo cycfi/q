b15c0ba827101670a1aee4712ece8511c7003433
djowel <djowel@gmail.com>
2023-05-22 15:20:07 +0800

    Tighten the promote_unit semantics

--- q_lib/include/q/synth/concepts.hpp
+++ q_lib/include/q/synth/concepts.hpp
@@ -17,48 +17,48 @@ namespace cycfi::q::concepts
       std::assignable_from<T&, T> &&
       std::default_initializable<T> &&
       requires(T o, T a, T b, phase_iterator pi)
-   {
-      o(pi);            // Generate a periodic waveform given `{phase_iterator}`, `pi`.
-   };
+      {
+         o(pi);            // Generate a periodic waveform given `{phase_iterator}`, `pi`.
+      };
 
    template <typename T>
    concept BasicOscillator =
       Oscillator<T> &&
       requires(T o, phase ph)
-   {
-      o(ph);            // Generate a periodic waveform given `{phase}`, `pi`.
-   };
+      {
+         o(ph);            // Generate a periodic waveform given `{phase}`, `pi`.
+      };
 
    template <typename T>
    concept BandwidthLimitedOscillator =
       Oscillator<T> &&
       requires(T o, phase ph, phase dt)
-   {
-      o(ph, dt);        // Generate a periodic waveform given `{phase}`, `pi`
-                        // and another `{phase}`, `dt` representing the delta
-                        // phase between two samples of the waveform (this is
-                        // equivalent to the `_step` member function of the
-                        // `{phase_iterator}`). (ph))`
-   };
+      {
+         o(ph, dt);        // Generate a periodic waveform given `{phase}`, `pi`
+                           // and another `{phase}`, `dt` representing the delta
+                           // phase between two samples of the waveform (this is
+                           // equivalent to the `_step` member function of the
+                           // `{phase_iterator}`). (ph))`
+      };
 
    template <typename T>
    concept Generator =
       std::copy_constructible<T> &&
       std::assignable_from<T&, T> &&
       requires(T g, T a, T b)
-   {
-      g();              // Generate a signal.
-   };
+      {
+         g();              // Generate a signal.
+      };
 
    template <typename T>
    concept Ramp =
       Generator<T> &&
       requires(T v, duration w, float sps)
-   {
-      T(w, sps);        // Construct a `Ramp` given `duration`, `w`, and `sps`.
-      v.reset();        // Reset the Ramp to the start.
-      v.config(w, sps); // Configure a `Ramp` given `duration`, `w`, and `sps`.
-   };
+      {
+         T(w, sps);        // Construct a `Ramp` given `duration`, `w`, and `sps`.
+         v.reset();        // Reset the Ramp to the start.
+         v.config(w, sps); // Configure a `Ramp` given `duration`, `w`, and `sps`.
+      };
 }
 
 #endif
